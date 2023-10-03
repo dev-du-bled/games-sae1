38997517c76c5f3ea9adbd61364c6dbf326adf16
Zarox28 <kujawskialexian@gmail.com>
2023-10-03 19:07:45 +0200

    change main indent to 2 spaces

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include "libs/termkit.cpp"
 
 using namespace std;
-int main()
-{
-    printf("%sRed text%s", rgb_fg(255,0,0).c_str(), DEFAULT_TERM_STYLE);
-    move_cursor_left(5);
-    cout<<"Lorem ipsum dolor sit ammet";
-    clear_line();
-    cout<<"He"<<endl;
-    return 0;
+int main() {
+  printf("%sRed text%s", rgb_fg(255, 0, 0).c_str(), DEFAULT_TERM_STYLE);
+  move_cursor_left(5);
+  cout << "Lorem ipsum dolor sit ammet";
+  clear_line();
+  cout << "He" << endl;
+  return 0;
 }
