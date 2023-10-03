5547862f24df1bee42fad5f87ca72bf3d365dbeb
Zarox28 <kujawskialexian@gmail.com>
2023-10-03 21:15:59 +0200

    tictactoe - Change class to struct

--- games/tictactoe/main.cpp
+++ games/tictactoe/main.cpp
@@ -1,55 +1,54 @@
 #include <iostream>
 #include "../../libs/termkit.cpp"
 
-class Player {
-  public:
-    std::string name;
-    char symbol;
-    int score;
+struct Player {
+  std::string name;
+  char symbol;
+  int score;
 
-    void player(std::string name, char symbol) {
-      this->name = name;
-      this->symbol = symbol;
-      this->score = 0;
-    }
+  void player(std::string name, char symbol) {
+    this->name = name;
+    this->symbol = symbol;
+    this->score = 0;
+  }
 
-    void addScore() {
-      this->score++;
-    }
+  void addScore() {
+    this->score++;
+  }
 
-    void resetScore() {
-      this->score = 0;
-    }
+  void resetScore() {
+    this->score = 0;
+  }
 };
 
-class Board {
-  public:
-    int size = 3;
-    char grid[3][3];
+struct Board {
+  int size = 3;
+  char grid[3][3];
 
-    void createBoard(int size) {
-      this->size = size;
+  void createBoard(int size) {
+  this->size = size;
+    this->grid[size][size];
+  }
+
+  void resetBoard() {
+    for (int i = 0; i < this->size; i++) {
+      for (int j = 0; j < this->size; j++) {
+        this->grid[i][j] = ' ';
+      }
     }
+  }
 
-    void resetBoard() {
-      for (int i = 0; i < this->size; i++) {
-        for (int j = 0; j < this->size; j++) {
-          this->grid[i][j] = ' ';
+  bool isFull() {
+    for (int i = 0; i < this->size; i++) {
+      for (int j = 0; j < this->size; j++) {
+        if (this->grid[i][j] == ' ') {
+          return false;
         }
       }
     }
-
-    bool isFull() {
-      for (int i = 0; i < this->size; i++) {
-        for (int j = 0; j < this->size; j++) {
-          if (this->grid[i][j] == ' ') {
-            return false;
-          }
-        }
-      }
-
-      return true;
-    }
+    
+    return true;
+  }
 };
 
 int main() {
