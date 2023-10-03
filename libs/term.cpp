863f934d7d17267f41f2ec405c959e6ec0236ae8
EMPTY <testerthe60@gmail.com>
2023-10-03 09:04:30 +0200

    ➕ Created term lib, added rgb functions

---
+++ libs/term.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+extern const char DEFAULT_TERM_STYLE[5] = "\e[0m";
+
+std::string rgb_impl(unsigned r, unsigned g, unsigned b, bool color_background) {
+  char buffer[20];
+  snprintf(buffer,sizeof(buffer) , "\e[%i;2;%i;%i;%im",(38+color_background*10), r,g,b);
+  // Explicit casting from char array to cpp string to avoid being confused
+  return (std::string)buffer;
+}
+
+extern std::string rgb_fg(unsigned r, unsigned g, unsigned b) {
+  return rgb_impl(r,g,b, false);
+}
+
+extern std::string rgb_bg(unsigned r, unsigned g, unsigned b) {
+  return rgb_impl(r,g,b,true);
+}
+
