8ba61888327ae686f047603653ab24ce967d46df
Zarox28 <kujawskialexian@gmail.com>
2023-10-03 20:06:03 +0200

    First commit of tictactoe

---
+++ games/tictactoe/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include "../../libs/termkit.cpp"
+
+class Player {
+  public:
+    std::string name;
+    char symbol;
+    int score;
+
+    void player(std::string name, char symbol) {
+      this->name = name;
+      this->symbol = symbol;
+      this->score = 0;
+    }
+
+    void addScore() {
+      this->score++;
+    }
+
+    void resetScore() {
+      this->score = 0;
+    }
+};
+
+class Board {
+  public:
+    int size = 3;
+    char grid[3][3];
+
+    void createBoard(int size) {
+      this->size = size;
+    }
+
+    void resetBoard() {
+      for (int i = 0; i < this->size; i++) {
+        for (int j = 0; j < this->size; j++) {
+          this->grid[i][j] = ' ';
+        }
+      }
+    }
+
+    bool isFull() {
+      for (int i = 0; i < this->size; i++) {
+        for (int j = 0; j < this->size; j++) {
+          if (this->grid[i][j] == ' ') {
+            return false;
+          }
+        }
+      }
+
+      return true;
+    }
+};
+
+int main() {
+  return 0;
+}
