75044f4327a8b5558b3f88811b182f3541649660
Zarox28 <kujawskialexian@gmail.com>
2023-10-03 21:27:31 +0200

    In TicTacToe : - Char to Unsigned char - Struct to class

--- games/tictactoe/main.cpp
+++ games/tictactoe/main.cpp
@@ -1,54 +1,56 @@
 #include <iostream>
 #include "../../libs/termkit.cpp"
 
-struct Player {
-  std::string name;
-  char symbol;
-  int score;
+class Player {
+  public:
+    std::string name;
+    unsigned char symbol;
+    int score;
 
-  void player(std::string name, char symbol) {
-    this->name = name;
-    this->symbol = symbol;
-    this->score = 0;
-  }
+    void player(std::string name, unsigned char symbol) {
+      this->name = name;
+      this->symbol = symbol;
+      this->score = 0;
+    }
 
-  void addScore() {
-    this->score++;
-  }
+    void addScore() {
+      this->score++;
+    }
 
-  void resetScore() {
-    this->score = 0;
-  }
+    void resetScore() {
+      this->score = 0;
+    }
 };
 
-struct Board {
-  int size = 3;
-  char grid[3][3];
+class Board {
+  public:
+    int size = 3;
+    unsigned char grid[3][3];
 
-  void createBoard(int size) {
-  this->size = size;
-    this->grid[size][size];
-  }
-
-  void resetBoard() {
-    for (int i = 0; i < this->size; i++) {
-      for (int j = 0; j < this->size; j++) {
-        this->grid[i][j] = ' ';
-      }
+    void createBoard(int size) {
+      this->size = size;
+      this->grid[size][size];
     }
-  }
 
-  bool isFull() {
-    for (int i = 0; i < this->size; i++) {
-      for (int j = 0; j < this->size; j++) {
-        if (this->grid[i][j] == ' ') {
-          return false;
+    void resetBoard() {
+      for (int i = 0; i < this->size; i++) {
+        for (int j = 0; j < this->size; j++) {
+          this->grid[i][j] = ' ';
         }
       }
     }
-    
-    return true;
-  }
+
+    bool isFull() {
+      for (int i = 0; i < this->size; i++) {
+        for (int j = 0; j < this->size; j++) {
+          if (this->grid[i][j] == ' ') {
+            return false;
+          }
+        }
+      }
+
+      return true;
+    }
 };
 
 int main() {
