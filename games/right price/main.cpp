038f682300331f47014d54b4ef92935e2a5e404a
EMPTY <testerthe60@gmail.com>
2023-10-06 11:34:14 +0200

    🛠️ Random fixes, hopefully get_term_size works for winshit now

--- games/right price/main.cpp
+++ games/right price/main.cpp
@@ -12,11 +12,11 @@ public:
   }
 };
 
-int get_user_input(int min, int max, int price) {
+int get_user_input(int min, int max, std::string name) {
   int result;
 
   std::cout << "Combien pour "
-            << rgb_fg(std::to_string(price), 125, 56, 89) << " ? ("
+            << rgb_fg(name, 125, 56, 89) << " ? ("
             << rgb_fg(std::to_string(min), 24, 103, 255) << " - "
             << rgb_fg(std::to_string(max), 24, 103, 255) << " )";
 
@@ -27,11 +27,11 @@ int get_user_input(int min, int max, int price) {
 extern void justeprix() {
   bool won = false;
   int user_guess;
-  Object to_guess[1] = {Object("Awacs", 12)};
+  Object to_guess[1] = {Object("un Awacs", 12)};
   int price_to_guess =  to_guess[0].price;
   
   do {
-    user_guess = get_user_input(0, 300, price_to_guess);
+    user_guess = get_user_input(0, 300, to_guess[0].name);
     if (user_guess < price_to_guess) {
       printf("Higher");
     }
